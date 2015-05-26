561da9483bbf835d37501065249834d464983a4f
Franklin Hu <franklin@stripe.com>
2015-05-26 11:10:58 -0400

    storehaus-kafka-08: convert to scalatest

--- storehaus-kafka-08/src/test/scala/com/twitter/storehaus/kafka/KafkaAvroSinkSpec.scala
+++ storehaus-kafka-08/src/test/scala/com/twitter/storehaus/kafka/KafkaAvroSinkSpec.scala
@@ -16,7 +16,7 @@
 
 package com.twitter.storehaus.kafka
 
-import org.specs2.mutable.Specification
+import org.scalatest.{WordSpec, Matchers, OneInstancePerTest}
 import kafka.DataTuple
 import java.util.Date
 import com.twitter.util.{Future, Await}
@@ -29,13 +29,13 @@ import kafka.serializer.Decoder
  * @author Mansur Ashraf
  * @since 12/8/13
  */
-class KafkaAvroSinkSpec extends Specification {
+class KafkaAvroSinkSpec extends WordSpec with Matchers with KafkaContext with OneInstancePerTest {
   "KafkaAvroSink" should {
-    "put avro object on a topic" in new KafkaContext {
+    "put avro object on a topic" in pendingUntilFixed {
       val topic = "avro-topic-" + random
       val sink = KafkaAvroSink[DataTuple](Seq(broker), topic,executor)
         .filter {
-        case (k, v) => v.getValue % 2 == 0
+        case (k, v) => v.getValue % 2 == (0)
       }
 
       val futures = (1 to 10)
@@ -47,13 +47,13 @@ class KafkaAvroSinkSpec extends Specification {
       try {
         val stream = consumer.createMessageStreamsByFilter(new Whitelist(topic), 1,implicitly[Decoder[String]], implicitly[Decoder[DataTuple]])(0)
         val iterator = stream.iterator()
-        iterator.next().message.getValue % 2 === 0
-        iterator.next().message.getValue % 2 === 0
-        iterator.next().message.getValue % 2 === 0
-        iterator.next().message.getValue % 2 === 0
+        iterator.next().message.getValue % 2 should equal (0)
+        iterator.next().message.getValue % 2 should equal (0)
+        iterator.next().message.getValue % 2 should equal (0)
+        iterator.next().message.getValue % 2 should equal (0)
       } catch {
-        case e: ConsumerTimeoutException => failure("test failed as consumer timed out without getting any msges")
+        case e: ConsumerTimeoutException => fail("test failed as consumer timed out without getting any msges")
       }
-    }.pendingUntilFixed
+    }
   }
 }
